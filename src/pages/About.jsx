42234bd873f3b24903f4c35a8394883db2085b07
hsiehto <hsieh.tom@gmail.com>
2025-04-13 20:06:07 -0700

    initial about page information

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,13 @@
 function About() {
-  return <div className="container">about page</div>;
+  return (
+    <div className="container">
+      <div>
+        Hello! My name is <span className="bold-name">Tom Hsieh</span>,
+        currently looking for new full stack opportunities. I hope to bring my
+        unique background in marketing & computer science to my new role.
+      </div>
+    </div>
+  );
 }
 
 export default About;
