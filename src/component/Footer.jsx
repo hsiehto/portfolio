35cc9aed36042826a3fbc12477db0ef276650e71
hsiehto <hsieh.tom@gmail.com>
2025-04-21 16:41:01 -0700

    updated footer & portfolio image

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,10 +5,11 @@ import SocialButtons from "./SocialButtons";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Tom Hsieh. All rights reserved.</p>
+      <SocialButtons />
       <p>
         <a href="mailto:hsieh.tom@gmail.com">hsieh.tom@gmail.com</a>
       </p>
+      <p>&copy; 2025 Tom Hsieh. All rights reserved.</p>
     </footer>
   );
 }
--- src/component/SocialButtons.jsx
+++ src/component/SocialButtons.jsx
@@ -5,31 +5,29 @@ import { FaLinkedin } from "react-icons/fa";
 
 export default function SocialButtons() {
   return (
-    <div>
-      <div className="social-buttons-container">
-        <a
-          className="social-button"
-          href="https://github.com/hsiehto"
-          aria-label="Github"
-          title="Github"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <VscGithubInverted className="github-icon" />
-          <span className="git-hub-text">Github</span>
-        </a>
-        <a
-          className="social-button"
-          href="https://www.linkedin.com/in/hsiehtom/"
-          aria-label="Linkedin"
-          title="Linkedin"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <FaLinkedin className="linkedin-icon" />
-          <span className="linkedin-text">Linkedin</span>
-        </a>
-      </div>
+    <div className="social-buttons-container">
+      <a
+        className="social-button"
+        href="https://github.com/hsiehto"
+        aria-label="Github"
+        title="Github"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <VscGithubInverted className="github-icon" />
+        <span className="git-hub-text">Github</span>
+      </a>
+      <a
+        className="social-button"
+        href="https://www.linkedin.com/in/hsiehtom/"
+        aria-label="Linkedin"
+        title="Linkedin"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <FaLinkedin className="linkedin-icon" />
+        <span className="linkedin-text">Linkedin</span>
+      </a>
     </div>
   );
 }
